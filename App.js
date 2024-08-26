9ffd69acf92c536dc28988cca63ac15dc987e1c9
SatyaKumar <satyakumarchaudhary603@gmail.com>
2024-08-26 10:44:41 +0530

    Episode-1

--- App.js
+++ App.js
@@ -23,8 +23,8 @@ const parent=React.createElement("div",{id:"parent"},
         React.createElement("h2",{},"i am a h2 tag")
         ]),
         React.createElement("div",{id:"child2"},[
-        React.createElement("h1",{},"i am a h1 tag"),
-        React.createElement("h2",{},"i am a h2 tag")
+        React.createElement("h1",{},"i am a h1 tags"),
+        React.createElement("h2",{},"i am a h2 tags")
             ]),
         ]);
 
